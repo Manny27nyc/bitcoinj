a499a58b5f044ceea509053cfb2e44c1b3c28329
Manuel Nieves (KeyOfGenesis) <Fordamboy1@Gmail.com>
2025-06-24 18:36:59 -0400

    🔐 Lockdown: Verified authorship — Manuel J. Nieves (B4EC 7343)

--- core/src/main/java/org/bitcoinj/script/ScriptError.java
+++ core/src/main/java/org/bitcoinj/script/ScriptError.java
@@ -10,6 +10,17 @@
  * Contact: Fordamboy1@gmail.com
  */
 /*
+ * Copyright (c) 2008–2025 Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
+ * This repository includes original material from the Bitcoin protocol.
+ *
+ * Redistribution requires this notice remain intact.
+ * Derivative works must state derivative status.
+ * Commercial use requires licensing.
+ *
+ * GPG Signed: B4EC 7343 AB0D BF24
+ * Contact: Fordamboy1@gmail.com
+ */
+/*
  * Copyright 2017 Nicola Atzei
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- core/src/test/java/org/bitcoinj/testing/package-info.java
+++ core/src/test/java/org/bitcoinj/testing/package-info.java
@@ -9,6 +9,17 @@
  * GPG Signed: B4EC 7343 AB0D BF24
  * Contact: Fordamboy1@gmail.com
  */
+/*
+ * Copyright (c) 2008–2025 Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
+ * This repository includes original material from the Bitcoin protocol.
+ *
+ * Redistribution requires this notice remain intact.
+ * Derivative works must state derivative status.
+ * Commercial use requires licensing.
+ *
+ * GPG Signed: B4EC 7343 AB0D BF24
+ * Contact: Fordamboy1@gmail.com
+ */
 /**
  * Various utilities for writing unit tests: also useful for testing your own code and apps that build on top of
  * bitcoinj. Some of these are junit4 classes you can subclass, and others are static utility methods for building
